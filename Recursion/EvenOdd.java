0787d13cb638fdf08998074f9ee9cbf7c349e322
Prateek Singh <prateeksingh3102000@gmail.com>
2024-11-20 23:08:24 +0530

    New questions

--- Recursion/EvenOdd.java
+++ Recursion/EvenOdd.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class EvenOdd {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to check even or odd: ");
+        int num = sc.nextInt();
+//       if (isEven(num) == true){
+//           System.out.println(num+ " is Even");
+//       }
+//       else {
+//           System.out.println(num+" is odd");
+//       }
+
+       /* ============================================ */
+        if (isEven(num)){
+            System.out.println(num+ " is even.");
+        }
+        else{
+            System.out.println(num+" is odd.");
+        }
+    }
+//    public static boolean isEven(int num){
+//        if (num == 0){
+//            return true;
+//        }
+//        else{
+//            return isOdd(num-1);
+//        }
+//    }
+//    public static boolean isOdd(int num){
+//        if (num == 0){
+//            return false;
+//        }
+//        else {
+//            return isEven(num-1);
+//        }
+//    }
+
+    /* =============================================================== */
+    public static boolean isEven(int num){
+        if(num == 0){
+            return true;
+        } else if (num == 1) {
+            return false;
+        }
+        else{
+            return isEven(num-2);
+        }
     }
 }
